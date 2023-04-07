069ab2392899795ca06e3c9e12a816178bcfc7aa
kesha_dev <araslanov@chiedocover.ru>
2023-04-07 22:23:27 +0300

    saving authorization

--- chromedriver/vk_auth.py
+++ chromedriver/vk_auth.py
@@ -5,21 +5,29 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from params import login, password
+import os
+
+cur_directory = os.getcwd()
 
 options = webdriver.ChromeOptions()
 
 userAgent = UserAgent()
 options.add_argument(f"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                      f"Chrome/112.0.0.0 Safari/537.36")
-
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument('--allow-profiles-outside-user-dir')
+options.add_argument('--enable-profile-shortcut-manager')
+options.add_argument(f"user-data-dir={cur_directory}/user")
+options.add_argument('--profile-directory=Profile 1')
 driver = webdriver.Chrome(
-    executable_path=r"C:\Users\user\PycharmProjects\pythonProject\chromedriver.exe",
+    executable_path=f"{cur_directory}/chromedriver.exe",
     options=options
 )
 
 try:
     driver.implicitly_wait(10)
     driver.get(url="https://vk.com")
+    time.sleep(10)
     email_input = driver.find_element(By.ID,"index_email")
     email_input.clear()
     email_input.send_keys(login)
@@ -31,6 +39,7 @@ try:
     pass_input.send_keys(password)
     pass_input.send_keys(Keys.ENTER)
     time.sleep(15)
+
 except Exception as ex:
     print(ex)
 finally:
